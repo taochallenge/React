af3122a96a927be8b0d409342979031ae3a9e9ad
taochallenge <1395051988@qq.com>
2020-10-04 16:38:20 +0800

    build commit

---
+++ build/precache-manifest.dad4cc9cfb6a8fe87847136a928e38c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06351ff5c660630b2e84422130a54d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc32e9450d867891c176",
+    "url": "/static/js/2.4a4c7e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a4c7e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa5f09d34a32c73cac2",
+    "url": "/static/js/main.7ae49cbd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
